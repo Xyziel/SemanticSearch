45f977bfaae5624aa590c5f491147c329ce0e8d2
Radosław Bujak <radobujak@wp.pl>
2022-05-18 17:04:56 +0200

    reading files and starting program

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-
-import dearpygui.dearpygui as dpg
 from SemanticSearch import SemanticSearch
-import dearpygui.demo as demo
 import os
-
 from SemanticSearchGUI import SemanticSearchGUI
+
 
 VECTORIZERS = ["Tfidf", "BoW"]
 DECOMPOSERS = ["SVD", "LDiA", "PCA"]
 DISTANCES = ["l1", "l2", "cosine"]
+
 
 def main():
     corpus = []
@@ -17,18 +15,8 @@ def main():
     ids = []
     directory = "texts"
 
-    # with open("NewYork.txt") as file:
-    #     corpus.append(file.read())
-    # with open("YorkAustralia.txt") as file:
-    #     corpus.append(file.read())
-    # with open("YorkEngland.txt") as file:
-    #     corpus.append(file.read())
-    # titles = ["NY", "YA", "YE"]
-    # urls = ["1", "2", "3"]
-
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
-        # checking if it is a file
         if os.path.isfile(f):
             with open(f, encoding="utf-8") as file:
                 content = file.read()
@@ -40,14 +28,6 @@ def main():
     ss = SemanticSearch(corpus, urls, titles, "l2", "tfidf", "pca", 2)
     gui = SemanticSearchGUI(DISTANCES, VECTORIZERS, DECOMPOSERS, ss)
     gui.run()
-
-    # dpg.create_context()
-    # dpg.create_viewport(title='Semantic Search Engine', width=700, height=500)
-    # demo.show_demo()
-    # dpg.setup_dearpygui()
-    # dpg.show_viewport()
-    # dpg.start_dearpygui()
-    # dpg.destroy_context()
 
 
 if __name__ == '__main__':
